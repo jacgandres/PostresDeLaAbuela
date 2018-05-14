e939e1639b16608892de87b0d3244cb85997d216
jaime andres carvajal <jacgandres@yopmail.com>
2018-05-14 16:12:42 -0500

    inicializando plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,54 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { UsuarioProvider } from '../providers/usuario/usuario';
+
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { Facebook } from '@ionic-native/facebook';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYfcqRXUiO3-6AJUUJvozc8Mb2ttukdrY",
+  authDomain: "postresdelaabula.firebaseapp.com",
+  databaseURL: "https://postresdelaabula.firebaseio.com",
+  projectId: "postresdelaabula",
+  storageBucket: "postresdelaabula.appspot.com",
+  messagingSenderId: "266657007622"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuthModule, AngularFireModule, AngularFireDatabase, AngularFireDatabaseModule,
+    UsuarioProvider,
+    Facebook
+    
+
   ]
 })
 export class AppModule {}
