1969143918b2931dcafbd0ac460e91e9e2f00bda
jaime andres carvajal <jacgandres@yopmail.com>
2018-05-20 13:25:12 -0500

    colores

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,13 @@
-import { Component , LOCALE_ID} from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 
-import { Producto, Pedido } from "../../Modelo/Modelo.Export";
+import { Producto, Pedido, Usuario } from "../../Modelo/Modelo.Export";
 
 import { UsuarioProvider, StorageUsuarioProvider, ProductosProvider } from "../../providers/providers.export";
 
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 import { DetalleProductoPage } from '../detalle-producto/detalle-producto';
- 
 
 @Component({
   selector: 'page-home',
@@ -17,34 +16,40 @@ import { DetalleProductoPage } from '../detalle-producto/detalle-producto';
 export class HomePage {
 
   public Productos: Producto[] = [];
-  public credencial: any = {}
+  public Usuario: Usuario = {};
   public pedidos: Pedido[] = [];
-  public pedido: Pedido = {};
   public detalleProductoPage = DetalleProductoPage;
 
   constructor(public navCtrl: NavController,
-              private usuarioStorage: StorageUsuarioProvider,
-              private usuarioProv: UsuarioProvider,
-              private platform: Platform,
-              private productoProv: ProductosProvider,
-              private screenOrientation: ScreenOrientation ) {
-                
+    private usuarioStorage: StorageUsuarioProvider,
+    private usuarioProv: UsuarioProvider,
+    private platform: Platform,
+    private productoProv: ProductosProvider,
+    private screenOrientation: ScreenOrientation) {
+
     this.iniciarHome();
     this.ObtenerProducto();
 
     console.log(this.screenOrientation.type);
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-  
+
   }
 
   iniciarHome() {
     console.log("Iniciando iniciarHome");
-     
+
     this.usuarioStorage.obtenerUsuario().then((result) => {
-      this.credencial = this.usuarioStorage.usuarioAutenticado.credenciales;
+      this.Usuario = this.usuarioStorage.usuarioAutenticado;
+      if (this.Usuario.pedidos == null) {
+        this.pedidos = [];
+      }
+      else {
+        this.pedidos = this.Usuario.pedidos;
+        console.log("Pedidos: "+this.Usuario.pedidos.length);
+      }
       console.log("iniciarHome obtenerUsuario: ");
     })
-      
+
   }
 
   ObtenerProducto(): any {
