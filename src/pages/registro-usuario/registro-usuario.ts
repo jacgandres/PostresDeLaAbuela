1d338787a22ba905e6aa3fc4c24dc5f297810076
jaime andres carvajal <jacgandres@yopmail.com>
2018-05-25 11:22:19 -0500

    insertar usuario desde usuario y calve

--- src/pages/registro-usuario/registro-usuario.ts
+++ src/pages/registro-usuario/registro-usuario.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CommunUtilidadesProvider } from '../../providers/commun-utilidades/commun-utilidades';
-import { Credenciales } from '../../Modelo/Credenciales';
+import { Credenciales, Usuario } from '../../Modelo/Modelo.Export';
+import { UsuarioProvider } from '../../providers/usuario/UsuarioProvider';
+import { TabsPage } from '../tabs/tabs';
 
- 
+
 
 @IonicPage()
 @Component({
@@ -12,37 +14,38 @@ import { Credenciales } from '../../Modelo/Credenciales';
 })
 export class RegistroUsuarioPage {
 
-  public Nombre="";
-  public Apellido="";
-  public Telefono="";
-  public Clave="";
-  public VerificarClave="";
-  public Correo="";
+  usuarioStorage: any;
+  public Nombre = "";
+  public Apellido = "";
+  public Telefono = "";
+  public Clave = "";
+  public VerificarClave = "";
+  public Correo = "";
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              private funcionesComunes:CommunUtilidadesProvider) {
-                
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private usuarioProv: UsuarioProvider,
+    private funcionesComunes: CommunUtilidadesProvider) {
+
   }
 
   ionViewWillEnter() {
     console.log('ionViewDidLoad RegistroUsuarioPage');
-   
+
     let data = this.navParams.get('Data')
     console.log(JSON.stringify(data));
 
-    var result1= this.funcionesComunes.Encriptar(data.Clave)
- 
+    var result1 = this.funcionesComunes.Encriptar(data.Clave)
+
     console.log(JSON.stringify(result1.toString()));
-    
+
     this.Clave = data.Clave;
     this.Correo = data.Email;
     this.Telefono = data.Telefono;
   }
 
-  Registrarse(){
-    if(this.Clave != this.VerificarClave)
-    {
+  Registrarse() {
+    if (this.Clave != this.VerificarClave) {
       this.funcionesComunes.MostrarMensaje(
         "Información",
         "Las claves no coinciden",
@@ -54,11 +57,10 @@ export class RegistroUsuarioPage {
       );
       return;
     }
-    if( this.Apellido.length<1 ||
-        this.Nombre.length<1 ||
-        this.Correo.length<1 || 
-        this.Clave.length<1  )
-    { 
+    if (this.Apellido.length < 1 ||
+      this.Nombre.length < 1 ||
+      this.Correo.length < 1 ||
+      this.Clave.length < 1) {
       this.funcionesComunes.MostrarMensaje(
         "Información",
         "Todos los campos deben estar diligenciados",
@@ -71,7 +73,38 @@ export class RegistroUsuarioPage {
       return;
     }
 
+    let usuario: any = {};
+    usuario.displayName = this.Nombre + " " + this.Apellido;
+    usuario.email = this.Correo;
+    usuario.photoURL = "";
+    usuario.uid = this.funcionesComunes.guid();
+    usuario.phoneNumber = this.getPhoneNumber();
+
+    this.salvarCredencialEnFireBase(usuario, "Usuario/Clave", this.funcionesComunes.Encriptar(this.Clave).toString());
 
   }
 
+  private salvarCredencialEnFireBase(user: any, provider: string, clave: string) {
+    this.usuarioProv.cargarUsuario(clave,
+      user.displayName,
+      user.email,
+      user.photoURL,
+      user.uid,
+      provider,
+      true,
+      user.phoneNumber);
+
+    console.log("antes de entrar a la promesa firebase");
+    this.usuarioProv.salvarCredencialEnFireBase().then(() => {
+      debugger;
+      console.log("entro a la  promesa firebase");
+      this.usuarioStorage.guardarUsuario(this.usuarioProv.usuario);
+      this.navCtrl.setRoot(TabsPage);
+    });
+  }
+
+
+  getPhoneNumber(): any {
+    return "1234567890";
+  }
 }
